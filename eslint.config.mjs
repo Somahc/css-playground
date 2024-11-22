1c9e412057fa7cf00a4a61d91e27ea2ef80deb74
Soma Sekine <a8121151@aoyama.jp>
2024-11-22 18:43:56 +0900

    よくわからん

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
 
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -17,6 +19,32 @@ export default [
     {
         ignores: ['node_modules'],
     },
-    ...compat.extends('eslint:recommended'),
+    js.configs.recommended,
     eslintConfigPrettier,
+    {
+        files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+        languageOptions: {
+            parser: tsParser,
+        },
+        rules: {
+            ...ts.configs.recommended.rules,
+            ...ts.configs['eslint-recommended'].rules,
+        },
+    },
+    // js.configs.recommended,
+    // eslintConfigPrettier,
+    // ...compat.extends('eslint:recommended'),
+    // {
+    //     files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+    //     languageOptions: {
+    //         parser: tsParser,
+    //     },
+    //     plugins: {
+    //         ...ts.configs.recommended.plugins,
+    //     },
+    //     rules: {
+    //         ...ts.configs.recommended.rules,
+    //         ...ts.configs['eslint-recommended'].rules,
+    //     },
+    // },
 ]
