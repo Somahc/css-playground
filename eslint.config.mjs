54fbf40be7ebfb2636e8800727a3c42071a6066a
Soma Sekine <a8121151@aoyama.jp>
2024-11-23 01:28:07 +0900

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,19 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import typescriptEslintParser from '@typescript-eslint/parser'
+import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import { FlatCompat } from '@eslint/eslintrc'
-import eslintConfigPrettier from 'eslint-config-prettier'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-})
-
-export default [
-    {
-        languageOptions: {
-            parser: typescriptEslintParser,
-            parserOptions: {
-                project: true,
-                sourceType: 'module',
-            },
-        },
-    },
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
-    {
-        rules: {
-            'no-unused-vars': 'warn',
-            'no-undef': 'warn',
-            '@typescript-eslint/prefer-includes': 'error',
-        },
-    },
-]
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    // {
+    //     languageOptions: {
+    //         parserOptions: {
+    //             projectService: true,
+    //             tsconfigRootDir: import.meta.dirname,
+    //         },
+    //     },
+    // },
+    // {
+    //     files: ['**/*.js'],
+    //     extends: [tseslint.configs.disableTypeChecked],
+    // },
+)
